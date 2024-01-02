60e13ed1c29ee8855f65cfd44570b5bdca5dca37
President-Asher <Asher.renzenbrink@gmail.com>
2024-01-02 10:07:25 -0500

    ver2

--- Celsius to Fahrenheit and Kelvin.py
+++ Celsius to Fahrenheit and Kelvin.py
@@ -1,16 +1,29 @@
-print("What is the tempurature?:")
-asktemp = float(input())
-
-classoftemp = input("Is that in Celcius or Fahrenheit?: ")
-if classoftemp == 'Celsius' or 'celsius':
-    print("Fahrenheit:")
-    print(asktemp * 9/5 + 32)
-    print("Kelvin:")
-    print(asktemp + 273.15)
-elif classoftemp == 'Fahrenheit' or 'fahrenheit':
-    print("Celsius: ")
-    print((asktemp - 32) * 5/9)
-    print("Kelvin:")
-    print((asktemp - 32) * 5/9 + 273.15)
-else:
-    print("That is not a valid Unit of Measurement...")
+classoftemp = ''
+asktemp = float
+def Calculator():
+    print("What is the tempurature?:")
+    asktemp = float(input())
+    print("Is that in Celcius, Fahrenheit, or Kelvin?")
+    classoftemp = input("(Type C, F, or K): ")
+    if classoftemp == 'C':
+        print("Fahrenheit:")
+        print(asktemp * 9/5 + 32)
+        print("Kelvin:")
+        print(asktemp + 273.15)
+        Calculator()
+    elif classoftemp == 'F':
+        print("Celsius: ")
+        print((asktemp - 32) * 5/9)
+        print("Kelvin:")
+        print((asktemp - 32) * 5/9 + 273.15)
+        Calculator()
+    elif classoftemp == 'K':
+        print("Celsius:")
+        print(asktemp - 273.15)
+        print("Fahrenheit:")
+        print((asktemp - 273.15) * 9/5 + 32)
+        Calculator()
+    else:
+        print("That is not a valid Unit of Measurement...")
+        Calculator()
+Calculator()
